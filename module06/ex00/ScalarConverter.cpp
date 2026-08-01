c7b3a5904ef17ebdb10ad43c259405da45e08ddd
pitoneverde <pitoneverde181@gmail.com>
2026-08-01 19:15:18 +0200

    parsing logic

---
+++ module06/ex00/ScalarConverter.cpp
@@ -0,0 +1,165 @@
+#include "ScalarConverter.hpp"
+
+// BOILERPLATE
+
+ScalarConverter::ScalarConverter() {}
+ScalarConverter::ScalarConverter(const ScalarConverter& other) {(void)other;}
+ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other) {
+	(void)other;
+	return *this;
+}
+ScalarConverter::~ScalarConverter() {}
+
+// HELPERS
+
+ScalarConverter::e_type ScalarConverter::detectType(const std::string& literal) {
+	// CHAR: must be exactly 3 chars: 'x' => quote + char + quote
+	if (literal.length() == 3 && literal[0] == '\'' && literal[2] == '\'')
+		return TYPE_CHAR;
+
+	// FLOAT: special pseudo-literals
+	if (literal == "-inff" || literal == "+inff" || literal == "nanf")
+		return TYPE_FLOAT;
+
+	// DOUBLE: special pseudo-literals
+	if (literal == "-inf" || literal == "+inf" || literal == "nan")
+		return TYPE_DOUBLE;
+	
+	// FLOAT: at least a digit and ends with 'f'
+	if (!literal.empty() && literal[literal.length() - 1] == 'f') {
+		bool hasDigit = false;
+		for (size_t i = 0; i < literal.length() - 1; ++i)
+		{
+			// defensive cast to avoid UB because isdigit takes an int representing an unsigned char or EOF
+			if (std::isdigit(static_cast<unsigned char>(literal[i]))) {
+				hasDigit = true;
+				break;
+			}
+		}
+		if (hasDigit) return TYPE_FLOAT;
+	}
+	
+	// DOUBLE: at least one digit and contains '.'
+	bool hasDot = false;
+	bool hasDigit = false;
+	for (size_t i = 0; i < literal.length(); ++i)
+	{
+		if (literal[i] == '.') hasDot = true;
+		if (std::isdigit(static_cast<unsigned char>(literal[i]))) hasDigit = true;
+	}
+	if (hasDot && hasDigit) return TYPE_DOUBLE;
+	
+	// INT: optional sign + digits only
+	size_t start = 0;
+	if (!literal.empty() && (literal[0] == '-' || literal[0] == '+')) start = 1;
+	bool allDigits = (start < literal.length());	// not only a '-' or '+'
+	for (size_t i = start; i < literal.length(); ++i)
+	{
+		if (!std::isdigit(static_cast<unsigned char>(literal[i]))) {
+			allDigits = false;
+			break;
+		}
+	}
+	if (allDigits) return TYPE_INT;
+	
+	// INVALID: everything else
+	return TYPE_INVALID;	
+}
+
+// input guaranteed to be 'x' by detectType
+char ScalarConverter::parseChar(const std::string& literal) {
+	return literal[1];
+}
+
+int ScalarConverter::parseInt(const std::string& literal) {
+	try {
+		char* end;
+		const char* str = literal.c_str();
+		long value = std::strtol(str, &end, 10);
+		if (end == str)
+			throw std::invalid_argument("no conversion");
+		if (*end !='\0')
+			throw std::invalid_argument("trailing chars");
+		if (value < std::numeric_limits<int>::min() || value > std::numeric_limits<int>::max())
+			throw std::out_of_range("overflow");	
+		return static_cast<int>(value);
+	}
+	catch(const std::exception& e) {
+		std::cerr << e.what() << '\n';
+		return 0;
+	}
+}
+
+float ScalarConverter::parseFloat(const std::string& literal) {
+	if (literal == "-inff")
+		return -std::numeric_limits<float>::infinity();
+	if (literal == "+inff")
+		return std::numeric_limits<float>::infinity();
+	if (literal == "nanf")
+		return std::numeric_limits<float>::quiet_NaN();
+	try {
+		// strip trailing 'f'
+		std::string num = literal.substr(0, literal.length() -1);
+	
+		char* end;
+		const char* str = num.c_str();
+		float f = std::strtof(str, &end);
+		
+		if (end == str)
+			throw std::invalid_argument("no conversion");
+		if (*end != '\0')
+			throw std::invalid_argument("trailing chars");
+		
+		return f;
+	} catch (std::exception& e) {
+		std::cerr << e.what() << '\n';
+		return .0f;
+	}
+}
+
+double ScalarConverter::parseDouble(const std::string& literal) {
+	if (literal == "-inf")
+		return -std::numeric_limits<double>::infinity();
+	if (literal == "+inf")
+		return std::numeric_limits<double>::infinity();
+	if (literal == "nan")
+		return std::numeric_limits<double>::quiet_NaN();
+	try {
+		char* end;
+		const char* str = literal.c_str();
+		double d = std::strtod(str, &end);
+		
+		if (end == str)
+			throw std::invalid_argument("no conversion");
+		if (*end != '\0')
+			throw std::invalid_argument("trailing chars");
+		
+		return d;
+	} catch (std::exception& e) {
+		std::cerr << e.what() << '\n';
+		return .0;
+	}
+}
+
+void ScalarConverter::printConversions(char c) {
+
+}
+void ScalarConverter::printConversions(int i) {
+
+}
+void ScalarConverter::printConversions(float f) {
+
+}
+void ScalarConverter::printConversions(double d) {
+
+}
+
+void ScalarConverter::printAllImpossible() {
+
+}
+
+// ENTRYPOINT
+
+void ScalarConverter::convert(const std::string& scalar) {
+
+}
---
+++ module06/ex00/ScalarConverter.hpp
@@ -0,0 +1,46 @@
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <string>
+#include <iostream>
+#include <limits>
+#include <cstdlib>
+#include <cctype>
+#include <stdexcept>
+#include <cmath>
+
+
+class ScalarConverter
+{
+private:
+	ScalarConverter();
+	ScalarConverter(const ScalarConverter& other);
+	ScalarConverter& operator=(const ScalarConverter& other);
+	~ScalarConverter();
+
+	enum e_type {
+		TYPE_CHAR,
+		TYPE_INT,
+		TYPE_FLOAT,
+		TYPE_DOUBLE,
+		TYPE_INVALID
+	};
+	static e_type detectType(const std::string& literal);
+
+	static char parseChar(const std::string& literal); 
+	static int parseInt(const std::string& literal);
+	static float parseFloat(const std::string& literal);
+	static double parseDouble(const std::string& literal);
+
+	static void printConversions(char c);
+	static void printConversions(int i);
+	static void printConversions(float f);
+	static void printConversions(double d);
+
+	static void printAllImpossible();
+
+public:
+	static void convert(const std::string& scalar);
+};
+
+#endif
